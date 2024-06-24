5a9b6a4bf646cc73b5e658fe7e8bc121c1562fc1
Duc Anh <137094647+Konta37@users.noreply.github.com>
2024-06-24 07:50:50 +0700

    add bt

---
+++ src/homework/b2/TimeExample.java
@@ -0,0 +1,10 @@
+package homework.b2;
+
+import java.time.LocalTime;
+
+public class TimeExample {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println(currentTime);
+    }
+}
